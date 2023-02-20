3ea2a4afeaaf38fccdb94f68ce2dcd8081327ba0
M4rced <marcedlll12@gmail.com>
2023-02-19 22:50:39 -0500

    .gitignore and spring project

---
+++ ubicoApi/src/main/java/com/uco/apireservas/UbicoApiApplication.java
@@ -0,0 +1,13 @@
+package com.uco.apireservas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbicoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UbicoApiApplication.class, args);
+    }
+
+}
---
+++ ubicoApi/src/test/java/com/uco/apireservas/UbicoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uco.apireservas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbicoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
